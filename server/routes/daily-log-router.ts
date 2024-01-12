c0ccbdfe28391bd6963841d40c99220ba1e343c7
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2024-01-12 13:20:37 +0100

    removed ts-nose in favor of typescript execute because of bugs

--- server/routes/daily-log-router.ts
+++ server/routes/daily-log-router.ts
@@ -1,1 +1,19 @@
 
+import express from 'express'
+import { updateLog, getAllLogs, getLogDetails } from '../controllers/daily-log-controller'
+import { authenticate } from '../middlewares/auth-middleware'
+import asyncHandler from 'express-async-handler'
+import { validateRequest } from '../middlewares/validation-middleware'
+import { mealValidator } from '../utils/validators'
+
+const dailyLogRouter = express.Router()
+
+dailyLogRouter.route('/').post(authenticate, validateRequest(mealValidator) updateLog)
+
+// Define the route for getting all daily logs
+dailyLogRouter.route('/api/dailyLogs').get(authenticate, asyncHandler(getAllLogs))
+
+// Define the route for getting a specific daily log
+dailyLogRouter.route('/api/dailyLogs/:logId').get(authenticate, asyncHandler(getLogDetails))
+
+export default dailyLogRouter
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,5 @@ export default {
 		extend: {}
 	},
 	plugins: [],
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html']
 }
