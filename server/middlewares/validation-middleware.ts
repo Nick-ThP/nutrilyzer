c6e210219a4f076eb97af4cd72499400a8953635
Nicklas <95386272+Nick-ThP@users.noreply.github.com>
2024-01-16 18:16:27 +0100

    hotfix for validation HOF-middleware

--- server/middlewares/validation-middleware.ts
+++ server/middlewares/validation-middleware.ts
@@ -4,15 +4,17 @@ import { HTTP_STATUS } from '../utils/http-messages'
 import { NextFunction, Response } from 'express'
 
 export function validateRequest(validator: ValidationChain[] | ValidationChain[][]) {
-	return (req: ExtendedRequest, res: Response, next: NextFunction) => {
+	return async (req: ExtendedRequest, res: Response, next: NextFunction) => {
 		const validationArray = Array.isArray(validator[0]) ? (validator as ValidationChain[][]).flat() : (validator as ValidationChain[])
 
-		validationArray.forEach(validation => validation.run(req))
+		for (let validation of validationArray) {
+			await validation.run(req)
+		}
 
 		const errors = validationResult(req)
 
 		if (!errors.isEmpty()) {
-			return res.status(HTTP_STATUS.BAD_REQUEST).json({ errors: errors.array() })
+			return res.status(HTTP_STATUS.BAD_REQUEST).json({ errors: errors.array().map(err => err.msg) })
 		}
 
 		next()
